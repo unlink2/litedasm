0d3331af367e900eb713827db0251582d2bb1099
Lukas Krickl <lukas@krickl.dev>
2023-02-10 20:11:03 +0100

    Added arch structs

--- src/core/arch/mod.rs
+++ src/core/arch/mod.rs
@@ -1,1 +1,24 @@
 pub mod a6502;
+
+/// A match pattern
+pub enum Pattern {
+    Exact(u8),
+    And(u8),
+    Any,
+}
+
+impl Pattern {
+    pub fn is_match(&self, byte: u8) -> bool {
+        match self {
+            Self::Exact(b) => *b == byte,
+            Self::And(b) => *b & byte != 0,
+            Self::Any => true,
+        }
+    }
+}
+
+type PatternList = Vec<Pattern>;
+
+pub struct ArchDef {
+    patterns: Vec<PatternList>,
+}
