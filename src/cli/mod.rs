903ae57fddf3621431c99a1d4528e2569e041ac5
Lukas Krickl <lukas@krickl.dev>
2023-02-14 18:26:19 +0100

    Generalized 6502 opcode definition

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -18,7 +18,7 @@ mod test {
     use super::*;
     use crate::core::dasm::arch::Arch;
 
-    fn collect_arch(arch: &Arch, data: &[u8], expected: &str) {
+    fn test_arch_result(arch: &Arch, data: &[u8], expected: &str) {
         let mut result = "".to_string();
         arch.disas(
             |s, _arch, _ctx| {
@@ -34,7 +34,7 @@ mod test {
 
     #[test]
     fn a6502() {
-        collect_arch(
+        test_arch_result(
             &a6502::ARCH,
             &[0xFF, 0xaa, 0x69, 0x02, 0x1],
             ".db ff\n.db aa\nadc #$02\n.db 01\n",
--- src/core/dasm/arch/a6502.rs
+++ src/core/dasm/arch/a6502.rs
@@ -14,6 +14,22 @@ lazy_static! {
     };
 }
 
+fn transform_immediate(map: &mut BTreeMap<String, TransformList>, name: &str) {
+    map.insert(
+        format!("{}_immediate", name),
+        vec![
+            Transform::String("adc #$".into()),
+            Transform::Abs(AbsOut {
+                offset: 1,
+                fmt: ValueTypeFmt::LowerHex(2),
+                data_type: DataType::U8,
+            }),
+            Transform::new_line(),
+            Transform::Consume(1),
+        ],
+    );
+}
+
 fn transforms() -> BTreeMap<String, TransformList> {
     let mut map = BTreeMap::default();
 
@@ -29,33 +45,24 @@ fn transforms() -> BTreeMap<String, TransformList> {
             Transform::new_line(),
         ],
     );
-
-    map.insert(
-        "adc_imm".into(),
-        vec![
-            Transform::String("adc #$".into()),
-            Transform::Abs(AbsOut {
-                offset: 1,
-                fmt: ValueTypeFmt::LowerHex(2),
-                data_type: DataType::U8,
-            }),
-            Transform::new_line(),
-            Transform::Consume(1),
-        ],
-    );
+    transform_immediate(&mut map, "adc");
 
     map
 }
 
+fn matcher1(op: u8, name: &str) -> Matcher {
+    Matcher {
+        patterns: vec![
+            PatternAt::new(Pattern::Exact(op), 0),
+            PatternAt::new(Pattern::Any, 1),
+        ],
+        transforms: name.into(),
+    }
+}
+
 fn patterns() -> MatcherList {
     vec![
-        Matcher {
-            patterns: vec![
-                PatternAt::new(Pattern::Exact(0x69), 0),
-                PatternAt::new(Pattern::Any, 1),
-            ],
-            transforms: "adc_imm".into(),
-        },
+        matcher1(0x69, "adc_immediate"),
         Matcher {
             patterns: vec![PatternAt::new(Pattern::Any, 0)],
             transforms: "define_byte".into(),
